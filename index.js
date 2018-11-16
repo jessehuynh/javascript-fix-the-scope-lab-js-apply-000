7541170cd4925ba531e69014c69d3f832614e799
Jesse Huynh <jesseca.melise@live.com>
2018-11-16 14:14:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
+
+add2(995)
 
 var funkyFunction = function() {
   return function() {
